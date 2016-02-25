d18df96f8ffc2c0ca712b7070943d59f59771f3e
Spencer Davis <spencerfdavis@gmail.com>
2016-02-24 20:08:37 -0700

    Remove articulate specific setup

--- src/lib/awsOptions.js
+++ src/lib/awsOptions.js
@@ -1,25 +1,13 @@
 
-const awsBaseOptions = {
-  apiVersion: '2012-08-10',
-  accesskeyid: process.env.AWS_ACCESS_KEY_ID,
-  secretaccesskey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_DEFAULT_REGION,
+const defaultApiVersion = '2012-08-10'
+
+const defaultAwsOptions = {
+  apiVersion: defaultApiVersion,
   retryHandler: (method, table) => {
     console.log('retrying method', method, table)
   }
 }
 
-export default function awsOptions(devOptions) {
-  let result
-
-  if (process.env.NODE_ENV === 'production') {
-    result = Object.assign({}, awsBaseOptions, devOptions)
-  } else {
-    result = Object.assign({}, awsBaseOptions, {
-      sslEnabled: false,
-      endpoint: 'http://:::45677'
-    }, devOptions)
-  }
-
-  return result
+export default function awsOptions(options) {
+  return Object.assign({}, awsBaseOptions, options)
 }
